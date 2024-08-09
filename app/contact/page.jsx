c02e956f4660735108214f9b5e298eb7eded86e2
Khalil <150041837+khaliiile@users.noreply.github.com>
2024-08-09 20:49:50 +0100

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import { Input } from "@/components/ui/Input";
+import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import {
